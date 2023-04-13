fd9ab4d14943501f67dbbe7489596c195e29f8fd
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2023-04-13 19:37:47 +0900

    [Bronze I] Title: 단어 공부, Time: 476 ms, Memory: 29060 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next().toUpperCase();
+
+        int[] counts = new int['Z' - 'A' + 1];
+        char[] alpha = new char['Z' - 'A' + 1];
+
+        for (int i = 0; i < alpha.length; i++) {
+            alpha[i] = (char)('A' + i);
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            counts[word.charAt(i) - 'A']++;
+        }
+ 
+        int max = -1; // counts ( >= 0 )
+        String popular = "";
+
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] > max) {
+                max = counts[i];
+                popular = Character.toString(alpha[i]);
+            }
+            else if (counts[i] == max) {
+                popular = "?";
+            }
+        }
+        System.out.println(popular);
+
+        scanner.close();
+    }
+}
