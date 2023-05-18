20134738d32d9e200dcd5c7f3354da697fddb798
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2023-05-18 20:17:00 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 5.35 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,6 +1,20 @@
-import java.util.Arrays;
-
 class Solution {
+    static int[][] matrix;
+    static int minNum;
+    
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = new int[queries.length];
+        matrix = initMatrix(rows, columns);
+        int queryIdx = 0;
+
+        for (int[] query : queries) {
+            rotation(query[0] - 1, query[1] - 1, query[2] - 1, query[3] - 1);
+            answer[queryIdx] = minNum;
+            queryIdx++;
+        }
+
+        return answer;
+    }
     
     public int[][] initMatrix(int rows, int columns) {
         int[][] matrix = new int[rows][columns];
@@ -14,51 +28,82 @@ class Solution {
         return matrix;
     }
     
-    public int[] solution(int rows, int columns, int[][] queries) {
-        int[] answer = new int[queries.length];
-        int[][] matrix = initMatrix(rows, columns);
-        int queryIdx = 0;
+    static void rotation(int x1, int y1, int x2, int y2) {
+        int saveNum = matrix[x1][y1];
+        minNum = saveNum;
 
-        for (int[] query : queries) {
-            int x1 = query[0] - 1;
-            int y1 = query[1] - 1;
-            int x2 = query[2] - 1;
-            int y2 = query[3] - 1;
-            int saveNum = matrix[x1][y1];
-            int minNum = saveNum;
-
-
-            // 왼쪽 : 위로 올리기
-            for (int i = x1 + 1; i <= x2; i++) {
-                matrix[i - 1][y1] = matrix[i][y1];
-                minNum = Math.min(matrix[i][y1], minNum);
-            }
-
-            // 아래 : 왼쪽으로 이동
-            for(int i = y1 + 1; i <= y2; i++) {
-                matrix[x2][i - 1] = matrix[x2][i];
-                minNum = Math.min(matrix[x2][i], minNum);
-            }
-
-            // 오른쪽 : 아래로 이동
-            for (int i = x2 - 1; i >= x1; i--) {
-                matrix[i + 1][y2] = matrix[i][y2];
-                minNum = Math.min(matrix[i][y2], minNum);
-            }
-
-            // 위쪽 : 오른쪽으로 이동
-            for (int i = y2 - 1; i >= y1; i--) {
-                matrix[x1][i + 1] = matrix[x1][i];
-                minNum = Math.min(matrix[x1][i], minNum);
-            }
-
-            matrix[x1][y1 + 1] = saveNum;
-            answer[queryIdx] = minNum;
-            queryIdx++;
+        // 왼쪽 : 위로 올리기
+        for (int i = x1 + 1; i <= x2; i++) {
+            matrix[i - 1][y1] = matrix[i][y1];
+            minNum = Math.min(matrix[i][y1], minNum);
         }
+
+        // 아래 : 왼쪽으로 이동
+        for(int i = y1 + 1; i <= y2; i++) {
+            matrix[x2][i - 1] = matrix[x2][i];
+            minNum = Math.min(matrix[x2][i], minNum);
+        }
+
+        // 오른쪽 : 아래로 이동
+        for (int i = x2 - 1; i >= x1; i--) {
+            matrix[i + 1][y2] = matrix[i][y2];
+            minNum = Math.min(matrix[i][y2], minNum);
+        }
+
+        // 위쪽 : 오른쪽으로 이동
+        for (int i = y2 - 1; i >= y1; i--) {
+            matrix[x1][i + 1] = matrix[x1][i];
+            minNum = Math.min(matrix[x1][i], minNum);
+        }
+
+        matrix[x1][y1 + 1] = saveNum;
+    }
+    
+//     public int[] solution(int rows, int columns, int[][] queries) {
+//         int[] answer = new int[queries.length];
+//         int[][] matrix = initMatrix(rows, columns);
+//         int queryIdx = 0;
+
+//         for (int[] query : queries) {
+//             int x1 = query[0] - 1;
+//             int y1 = query[1] - 1;
+//             int x2 = query[2] - 1;
+//             int y2 = query[3] - 1;
+//             int saveNum = matrix[x1][y1];
+//             int minNum = saveNum;
+
+
+//             // 왼쪽 : 위로 올리기
+//             for (int i = x1 + 1; i <= x2; i++) {
+//                 matrix[i - 1][y1] = matrix[i][y1];
+//                 minNum = Math.min(matrix[i][y1], minNum);
+//             }
+
+//             // 아래 : 왼쪽으로 이동
+//             for(int i = y1 + 1; i <= y2; i++) {
+//                 matrix[x2][i - 1] = matrix[x2][i];
+//                 minNum = Math.min(matrix[x2][i], minNum);
+//             }
+
+//             // 오른쪽 : 아래로 이동
+//             for (int i = x2 - 1; i >= x1; i--) {
+//                 matrix[i + 1][y2] = matrix[i][y2];
+//                 minNum = Math.min(matrix[i][y2], minNum);
+//             }
+
+//             // 위쪽 : 오른쪽으로 이동
+//             for (int i = y2 - 1; i >= y1; i--) {
+//                 matrix[x1][i + 1] = matrix[x1][i];
+//                 minNum = Math.min(matrix[x1][i], minNum);
+//             }
+
+//             matrix[x1][y1 + 1] = saveNum;
+//             answer[queryIdx] = minNum;
+//             queryIdx++;
+//         }
     
         
-        return answer;
-    }
+//         return answer;
+//     }
     
 }
