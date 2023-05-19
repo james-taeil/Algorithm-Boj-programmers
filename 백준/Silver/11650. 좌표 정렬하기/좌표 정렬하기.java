224ef3a803338ce73730a6e221426c03dc512523
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2023-05-19 13:31:22 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 872 ms, Memory: 57952 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (el1, el2) -> {
+            if (el1[0] == el2[0]) return el1[1] - el2[1];
+            return el1[0] - el2[0];
+        });
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+
+        System.out.println(sb);
+    }
+}
