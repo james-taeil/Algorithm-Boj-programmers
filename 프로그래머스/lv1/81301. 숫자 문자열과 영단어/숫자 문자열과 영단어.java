9d653050b06b88a9e74d6433016f71ce93346329
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2023-06-15 00:24:34 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String s) {
+        String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        for (int i = 0; i < words.length; i++) {
+            s = s.replace(words[i], num[i]);
+        }
+        return Integer.parseInt(s);
+    }
+}
