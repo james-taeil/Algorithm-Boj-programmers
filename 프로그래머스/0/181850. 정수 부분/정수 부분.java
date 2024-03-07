cd46c468862498e7b6e5f62cba0d00eb6a0cdb82
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2024-03-07 21:38:33 +0900

    [level 0] Title: 정수 부분, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+        
+        int INT = (int) flo;
+        
+        
+        
+        return INT;
+    }
+}
