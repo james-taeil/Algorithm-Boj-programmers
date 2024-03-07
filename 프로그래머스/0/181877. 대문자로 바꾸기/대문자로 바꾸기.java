15e952c0afade0962561bca9f4aaea4fcef8cd3c
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2024-03-07 21:41:02 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        String word = myString.toUpperCase();
+        
+        answer = word;
+        
+        return answer;
+    }
+}
