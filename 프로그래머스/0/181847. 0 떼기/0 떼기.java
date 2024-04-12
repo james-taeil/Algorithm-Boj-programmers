0004bc4ae8f8d8ac2393522987b105587571ef91
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2024-04-12 18:51:14 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int temp = 0;
+        
+        temp = Integer.valueOf(n_str);
+        
+        answer = Integer.toString(temp);
+        
+        return answer;
+    }
+}
