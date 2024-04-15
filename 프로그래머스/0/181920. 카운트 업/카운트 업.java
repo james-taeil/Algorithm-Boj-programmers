da82ae1626cf95c842eb5bb2c22d12c9828442be
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2024-04-15 19:38:03 +0900

    [level 0] Title: 카운트 업, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,8 +1,11 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[end_num - start_num + 1];
         
-        for (int i = 0; i < end_num - start_num + 1; i++) {
+        int range = end_num - start_num + 1;
+        
+        int[] answer = new int[range];
+        
+        for (int i = 0; i < range; i++) {
             answer[i] = start_num + i;
         }
         
