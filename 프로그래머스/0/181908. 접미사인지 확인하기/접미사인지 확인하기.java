14f5f85f94cb89152c1effbceb04c0bab67bdab2
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2024-05-14 13:19:25 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -5,7 +5,7 @@ class Solution {
         int len = my_string.length();
         
         for (int i = 0; i < len; i++) {
-            if (my_string.substring(i, len).equals(is_suffix)) {
+            if (my_string.substring(i).equals(is_suffix)) {
                 answer = 1;
                 break;
             }
