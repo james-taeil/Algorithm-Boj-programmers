02f07fda5972ca3ecbf52964fbdf4cc8c519fe34
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2024-05-16 10:09:06 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        
+        int len = my_string.length();
+        
+        for (int i = 0; i < len; i++) {
+            if (my_string.substring(0, i).equals(is_prefix)) {
+                answer = 1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
