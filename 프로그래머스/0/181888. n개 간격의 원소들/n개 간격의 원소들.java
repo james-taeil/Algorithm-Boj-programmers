eeab5bbd821d1a07f543596f47806b9b86a66c9b
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2024-06-05 11:27:52 +0900

    [level 0] Title: n개 간격의 원소들, Time: 1.93 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        ArrayList<Integer> arr = new ArrayList();
+        
+        for (int i = 0; i < num_list.length; i += n) {
+            arr.add(num_list[i]);
+        }
+        
+        answer = arr.stream().mapToInt(Integer :: intValue).toArray();
+        
+        return answer;
+    }
+}
