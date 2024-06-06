fbbb32e1fab66ddf90edea779d193539657f6369
Tae-il Kim <71359732+james-taeil@users.noreply.github.com>
2024-06-06 16:00:56 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        int even = 0;
+        int odd = 0;
+        int len = num_list.length;
+        
+        for (int i = 0; i < len; i++) {
+            if (i % 2 == 0) {
+                even += num_list[i];
+            } else {
+                odd += num_list[i];
+            }
+        }
+        
+        if (odd >= even) {
+            answer = odd;
+        } else {
+            answer = even;
+        }
+        
+        return answer;
+    }
+}
