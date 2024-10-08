d5de02fcb0720363dd65fb8ab6b2567e13fbb7ac
Tae-il Kim <roseswallow13@gmail.com>
2024-10-08 13:40:26 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 7.96 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = Integer.MAX_VALUE;
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 1) {
+                answer = Math.min(answer, i);
+            }
+        }
+        
+        return answer;
+    }
+}
