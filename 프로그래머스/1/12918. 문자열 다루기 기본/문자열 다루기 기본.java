75692476a2abb6d5ee9e5cc0713246d2150b65f2
Tae-il Kim <roseswallow13@gmail.com>
2024-10-15 09:32:21 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        int len = s.length();
+        
+        if (len != 4 && len != 6) {
+            return false;
+        }
+        
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') {
+                return false;
+            }
+        }
+        
+        return answer;
+    }
+}
