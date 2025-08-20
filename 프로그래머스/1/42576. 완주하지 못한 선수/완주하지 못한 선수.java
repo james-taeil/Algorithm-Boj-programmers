53cef2e95969109e74debc0f67e9d180efb4a9d8
Tae-il Kim <roseswallow13@gmail.com>
2025-08-20 18:11:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 113.25 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,21 +4,29 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        // 1. 두 리스트 sort
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        // hashmap을 활용하여 문제를 푼다면
+        HashMap<String, Integer> map = new HashMap<>();
         
-        // 2. completion list length만큼 돌면서 participant 확인해서 없는 선수 찾기
-        int len = completion.length;
+        // 1. 참가자 이름 카운트 늘리기
+        for (String player : participant) {
+            map.put(player, map.getOrDefault (player, 0) + 1);
+        }
         
-        for (int i = 0; i < len; i++) {
-            if (!participant[i].equals(completion[i])) {
-                return participant[i];
+        // 2. 완주한 참가자 이름 카운트 줄이기
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+        
+        
+        // 3. 마지막에 남은 hash가 완주하지 못한 선수
+        
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 0) {
+                return entry.getKey();
             }
         }
         
-        // 3. 전부 다 돌았는데 없을 경우 마지막 주자가 완주하지 못한 선수
              
-        return participant[participant.length - 1];
+        return "";
     }
 }
